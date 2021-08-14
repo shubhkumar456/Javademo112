5a6e8591eff3e968a18e52df83297ca9a08ea81b
shubhkumar456 <shubhkumar456@gmail.com>
2021-08-14 09:04:58 +0530

    variableconditions

---
+++ src/variablesconditions/Conditions.java
@@ -0,0 +1,33 @@
+package variablesconditions;
+
+public class Conditions {
+    public static void main(String args[]) {
+
+
+        int age = 34;
+        if (age > 12) {
+            System.out.println("the person is young..........");
+        }
+        else{
+            System.out.println("the person is old.....");
+        }
+        double price=32234.4;
+
+         if ( price<23333.0){
+             System.out.println("my salary is more than other...... ");
+        }
+        else{
+            System.out.println("my salary is not more than other...... ");
+
+        }
+        String project="Pythonk";
+        if (project=="Java"){
+            System.out.println("my project is Java.......");
+        }
+        else{
+            System.out.println("my project is not Java.......");
+
+        }
+
+    }
+}
