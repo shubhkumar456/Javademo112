5ccc5886841f9a383c44f54fd626165c0259dcef
shubhkumar456 <shubhkumar456@gmail.com>
2021-08-17 16:02:48 +0530

    variablesdemo4

---
+++ src/variables/variabledemo4.java
@@ -0,0 +1,31 @@
+package variables;
+
+public class variabledemo4 {
+    public static void main(String[] args) {
+
+        String name="Shubendra kumar";
+        System.out.println("name");
+        System.out.println("my name is ------"+name);
+
+        int age= 24;
+        System.out.println("age");
+        System.out.println("my age is-----"+age);
+
+        double salary= 50000.5;
+        System.out.println(salary);
+        System.out.println("my monthly salary is----"+salary);
+
+        String brand ="oneplus";
+        System.out.println(brand);
+        System.out.println("iam using this mobile phone--"+brand);
+
+        double price=17999.5;
+        System.out.println(price);
+        System.out.println("the price of this mobile phone is---"+ price);
+
+        int tools =3;
+        System.out.println(tools);
+        System.out.println("it contains number of tools is---"+tools);
+
+    }
+}
