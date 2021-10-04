96694784617c26a49c208163bae851dc61761d8b
shubhkumar456 <shubhkumar456@gmail.com>
2021-10-04 12:53:45 +0530

    Revision

---
+++ src/iforelseconditionday2/iforelseday3.java
@@ -0,0 +1,69 @@
+package iforelseconditionday2;
+
+public class iforelseday3 {
+
+    public static void main(String[] args) {
+
+        int age =14;
+        String name = "shubendra";
+
+        if (age>20 && name =="shubendra"){
+            System.out.println("this is a young boy and name is shubendra");
+        }
+        else{
+            System.out.println("This is not a young boy or not the name is shubendra");
+        }
+
+        int age1 = 13;
+        String main = "shubendra Kumar";
+
+        if (age1>21 || main =="Shubendra Kumar"){
+            System.out.println("Age1 is mine and name is shubendra kumar");
+        }
+        else{
+            System.out.println("Age1 is not mine and name is not shubendra kumar");
+        }
+
+        int my_age =30;
+        if(!(my_age>20 )){
+            System.out.println("print if loops ");
+        }
+        else{
+            System.out.println("print else loops ");
+        }
+
+        int num =101;
+        String nam ="aanya";
+        if (num>100 && nam=="aanya" ){
+            System.out.println("All Value is correct whinch i put in if loops");
+        }
+        else{
+            System.out.println("other wise all values are wrong in else loops");
+        }
+
+        int kilo= 74;
+        String Meter ="digital";
+        if (kilo>80 || Meter=="Digital"){
+            System.out.println("output comes in if loops");
+        }
+        else{
+            System.out.println("output comes in else loops");
+        }
+
+        int finger = 10;
+
+        if(!(finger>5)){
+            System.out.println("output comes in if loops");
+        }
+        else {
+            System.out.println("output comes in else loops");
+        }
+        }
+
+
+
+    }
+
+
+
+
