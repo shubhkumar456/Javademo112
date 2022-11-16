448a6849df0a7645eeadfbd133819adcf06a5743
LENOVO <shubhkumar456>
2022-11-16 13:21:06 +0530

    Practice

--- src/Oddeven.java
+++ src/Oddeven.java
@@ -1,17 +1,46 @@
-public class Oddeven {
-    public static void main(String[] args) {
-        int [] array={1,2,3,4,5,6,7,8,9,10};
-        System.out.println("odd number");
-        for (int i=0; i< array.length; i++){
-            if (array[i]%2!=0){
-                System.out.println(array[i]);
-            }
-        }
-        System.out.println("even numbers");
-        for (int i=0; i< array.length; i++){
-            if (array[i]%2==0){
-                System.out.println(array[i]);
-            }
-        }
-    }
-}
+import emparray.Employee;
+
+import java.util.Map;
+
+//public class Oddeven {
+//    public static void main(String[] args) {
+//
+//        Employee emp1= new Employee(1);
+//        Employee emp2= new Employee(1);
+//
+//        Map<Employee,String> map = Hashmap();
+//        map.put(emp1,"Interview");
+//        map.put(emp2,"Interview");
+//
+//        System.out.print(map.size()) //guess it Size : ?
+//
+//
+//        Map<Integer,String> map = Hashmap();
+//        map.put(1,"Interview")
+//        map.put(1,"Interview")
+//
+//        System.out.print(map.size()) //guess it Size : ?
+//
+//    }
+//
+////        int [] array={1,2,3,4,5,6,7,8,9,10};
+////        System.out.println("odd number");
+////        for (int i=0; i< array.length; i++){
+////            if (array[i]%2!=0){
+////                System.out.println(array[i]);
+////            }
+////        }
+////        System.out.println("even numbers");
+////        for (int i=0; i< array.length; i++){
+////            if (array[i]%2==0){
+////                System.out.println(array[i]);
+////            }
+////        }
+//    }
+//public class Employee{
+//
+//    int id; //@Getter //@Setter
+//}
+//
+//
+////}
--- src/reverseStringwithPalindrome/Revpalin.java
+++ src/reverseStringwithPalindrome/Revpalin.java
@@ -8,7 +8,7 @@ public class Revpalin {
         String reverse="";
         String org=name;
 
-        for (int i=name.length()-1;i>=0;i--){
+        for (int i=name.length()-1; i>=0; i--){
             reverse=reverse+name.charAt(i);
         }
         System.out.println(reverse);
